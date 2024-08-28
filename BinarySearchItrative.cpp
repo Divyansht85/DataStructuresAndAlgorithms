7bc42555db608e4774c6591aedaf343df1eb262a
Divyansh Tripathi <47769848+Divyansht85@users.noreply.github.com>
2024-08-28 19:59:36 +0530

    First and Last Occurance Binary Search

--- BinarySearchItrative.cpp
+++ BinarySearchItrative.cpp
@@ -14,8 +14,48 @@ bool binarySearch(int *a, int low, int high, int x)
     }
     return false;
 }
+int firstOccurance(int *a, int n, int x)
+{
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (x < a[mid])
+            high = mid - 1;
+        else if (x > a[mid])
+            low = mid + 1;
+        else
+        {
+            if (mid == 0 || a[mid - 1] != a[mid])
+                return mid;
+            else
+                high = mid - 1;
+        }
+    }
+    return -1;
+}
+int lastOccurance(int *a, int n, int x)
+{
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (x < a[mid])
+            high = mid - 1;
+        else if (x > a[mid])
+            low = mid + 1;
+        else
+        {
+            if (mid == n - 1 || a[mid] != a[mid + 1])
+                return mid;
+            else
+                low = mid + 1;
+        }
+    }
+    return -1;
+}
 int main()
 {
-    int arr[5] = {6, 7, 8, 9, 12};
-    cout << std::boolalpha << binarySearch(arr, 0, 4, 5) << endl;
+    int arr[10] = {1, 2, 3, 6, 6, 6, 6, 7, 8, 9};
+    cout << lastOccurance(arr, 10, 6) << endl;
 }
