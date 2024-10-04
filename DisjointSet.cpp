e6dfb3abd8f935d75515b6b3f00a34dafae14a31
Divyansh Tripathi <47769848+Divyansht85@users.noreply.github.com>
2024-10-04 18:15:27 +0530

    Updated DisjointSet Class

--- DisjointSet.cpp
+++ DisjointSet.cpp
@@ -2,15 +2,21 @@ class DisjointSet
 {
     int *size;
     int *parent;
+    int N;
 
 public:
-    DisjointSet(int n) : size(new int[n]), parent(new int[n])
+    DisjointSet(int n) : size(new int[n]), parent(new int[n]), N(n)
     {
         for (int i = 0; i < n; i++)
         {
             size[i] = 1;
             parent[i] = i;
         }
+    }
+    ~DisjointSet()
+    {
+        delete[] size;
+        delete[] parent;
     }
     int find(int x)
     {
@@ -36,8 +42,13 @@ public:
             size[x_rep] += size[y_rep];
         }
     }
-    bool isSameComponent(int x, int y)
+    bool isSameGroup(int x, int y) { return find(x) == find(y); }
+    int totalComponents()
     {
-        return find(x) == find(y);
+        int count = 0;
+        for (int i = 0; i < N; i++)
+            if (i == parent[i])
+                count++;
+        return count;
     }
 };
